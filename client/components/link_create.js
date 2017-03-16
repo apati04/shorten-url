53080e1a47da25b9746dcb6eb8cd1f68b16b2d3f
Andrew Patipaksiri <apax714@gmail.com>
2017-03-16 15:57:03 -0700

    made meteor application secure and able to attempt to save link input by user

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react';
 class LinkCreate extends Component {
   handleSumbit(e) {
     e.preventDefault();
-    console.log(this.refs.input.value);
+
+    Meteor.call('links.insert', this.refs.link.value);
   }
   render() {
     return (
       <form onSubmit={this.handleSumbit.bind(this)}>
         <div className="form-group">
           <label>Link to Shorten</label>
-          <input ref="input" className="form-control" />
+          <input ref="link" className="form-control" />
         </div>
         <button className="btn btn-primary">Shorten!</button>
       </form>
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,8 @@
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+  'links.insert': function(url) {
+    console.log('save: ', url);
+  }
+})
 export const Links = new Mongo.Collection('links');
