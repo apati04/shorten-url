0a333282ac75ffb7492ee8471b83000101102480
Andrew Patipaksiri <apax714@gmail.com>
2017-03-16 16:03:46 -0700

    add validation to url

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,10 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
 
 Meteor.methods({
   'links.insert': function(url) {
-    console.log('save: ', url);
+    check(url, Match.Where(url => validUrl.isUri(url)));
   }
 })
 export const Links = new Mongo.Collection('links');
